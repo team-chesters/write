19075ab72c9dfc4f45fab50e807e8bc914ea463d
che5ya <che5ya@naver.com>
2024-01-22 21:02:54 +0900

    # EDIT - add babel, jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
+  transform: {
+    '^.+\\.vue$': 'vue-jest',
+    '^.+\\.jsx?$': 'babel-jest'
+  },
+  testMatch: [
+    '**/tests/unit/**/*.spec.[jt]s?(x)',
+    '**/__tests__/*.[jt]s?(x)'
+  ],
+};
